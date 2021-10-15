a63bb6c04c89f05687a59970dca9120985c7b795
Windows10 <8090815@gmail.com>
2021-10-15 23:31:20 +0300

    init commit

---
+++ src/com/pb/nesterenko/hw2/calculator.java
@@ -0,0 +1,17 @@
+package com.pb.nesterenko.hw2;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+    int operand1;
+    int operand2;
+    String sign = "+"; // + - * /
+        operand1 = scan.nextInt();
+        operand2 = scan.nextInt();
+        System.out.println();
+
+    }
+
+}
---
+++ src/com/pb/nesterenko/hw2/interval.java
@@ -0,0 +1,23 @@
+package com.pb.nesterenko.hw2;
+
+import java.util.Scanner;
+
+public class interval {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int first, second;
+                System.out.print("Enter first num: ");
+                first = num.nextInt();
+
+                if (first <= 14) {
+                    System.out.print("Nan is one period");
+                    System.out.print("\n");
+                } else if (first >= 15 && first <= 35) {
+                    System.out.print("Num is two period");
+                } else if (first >= 36 && first <= 50) {
+                    System.out.print("num is three reriod");
+                } else if (first <= 51 && first >= 100) {
+                    System.out.print("Num is four period")
+                }
+    }
+}
